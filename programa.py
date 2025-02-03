2f70ac5e647cb00cbd5fe9c68a234f698e44c533
Sant44616 <santiagovelezsilva44@gmail.com>
2025-02-03 12:24:47 -0500

    menu y colores

--- programa.py
+++ programa.py
@@ -1,1 +1,23 @@
-print("hello")
+from subprocess import call 
+from color import Color 
+
+"--------------------------MENUS---------------------------------"
+def limpiar():
+    call("cls", shell = True)
+
+def Menu_Principal():
+    while True:
+        print(Color.CYAN)
+        menu = \
+        """
+                                                                Menu Principal 
+                                                        ----------------------------
+                                                        1. Agregar información.
+                                                        2. Modificar información. 
+                                                        3. Eliminar inoformación. 
+                                                        4. Consultar información
+                                                        5. Salir
+        """
+        print((menu))
+        print(Color.RESET)
+        option = input(f"Eliga la opción que desea: ".rjust(80))
